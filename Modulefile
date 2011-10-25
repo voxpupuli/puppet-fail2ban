b1a78c14c116720177e9b25bcb6f4a6c991e3538
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2011-10-25 09:34:56 +0200

    Initial commit

---
+++ Modulefile
@@ -0,0 +1,9 @@
+name 'dhoppe-fail2ban'
+version '0.0.1'
+
+author 'Dennis Hoppe'
+license 'GPLv2'
+project_page 'http://github.com/dhoppe/puppet-fail2ban'
+source 'git://github.com/dhoppe/puppet-fail2ban'
+summary 'Fail2Ban Module'
+description 'Fail2Ban Module for Debian, Ubuntu'
---
+++ manifests/init.pp
@@ -0,0 +1,62 @@
+class fail2ban {
+	define fail2ban::email($email = false, $whitelist = false) {
+		$t_email = $email ? {
+			false   => "root",
+			default => $email,
+		}
+
+		$t_whitelist = $whitelist ? {
+			false   => ["127.0.0.1"],
+			default => $whitelist,
+		}
+
+		file { "$name":
+			owner   => root,
+			group   => root,
+			mode    => 0644,
+			alias   => "jail.conf",
+			notify  => Service["fail2ban"],
+			content => template("fail2ban/$lsbdistcodename/etc/fail2ban/jail.conf.erb"),
+			require => Package["fail2ban"],
+		}
+	}
+
+	fail2ban::email { "/etc/fail2ban/jail.conf":
+		email     => "fail2ban@${domain}",
+		whitelist => [
+			"127.0.0.1",
+			"192.168.122.0/24"
+		],
+	}
+
+	file { "/etc/fail2ban/jail.local":
+		owner   => root,
+		group   => root,
+		mode    => 0644,
+		alias   => "jail.local",
+		notify  => Service["fail2ban"],
+		source  => [
+			"puppet:///modules/fail2ban/common/$hostname/etc/fail2ban/jail.local",
+			"puppet:///modules/fail2ban/common/etc/fail2ban/jail.local"
+		],
+		require => Package["fail2ban"],
+	}
+
+	package { "fail2ban":
+		ensure => present,
+	}
+
+	service { "fail2ban":
+		enable     => true,
+		ensure     => running,
+		hasrestart => true,
+		hasstatus  => true,
+		require    => [
+			File["jail.conf"],
+			File["jail.local"],
+			Package["fail2ban"]
+		],
+	}
+}
+
+# vim: tabstop=3
