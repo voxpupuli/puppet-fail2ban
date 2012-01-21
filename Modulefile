252f72d73a4af89ffa5a6a2ec67fc84ff720eb34
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-01-21 13:46:58 +0100

    Fix module layout

--- Modulefile
+++ Modulefile
@@ -1,5 +1,5 @@
 name 'dhoppe-fail2ban'
-version '0.0.5'
+version '0.0.6'
 
 author 'Dennis Hoppe'
 license 'GPLv2'
--- manifests/init.pp
+++ manifests/init.pp
@@ -15,7 +15,7 @@ class fail2ban {
 			group   => root,
 			mode    => 0644,
 			alias   => "jail.conf",
-			content => template("fail2ban/$::lsbdistcodename/etc/fail2ban/jail.conf.erb"),
+			content => template("fail2ban/${::lsbdistcodename}/etc/fail2ban/jail.conf.erb"),
 			notify  => Service["fail2ban"],
 			require => Package["fail2ban"],
 		}
@@ -35,7 +35,7 @@ class fail2ban {
 		mode    => 0644,
 		alias   => "jail.local",
 		source  => [
-			"puppet:///modules/fail2ban/$::hostname/etc/fail2ban/jail.local",
+			"puppet:///modules/fail2ban/${::hostname}/etc/fail2ban/jail.local",
 			"puppet:///modules/fail2ban/common/etc/fail2ban/jail.local"
 		],
 		notify  => Service["fail2ban"],
