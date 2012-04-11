07d2aebab29cfd8e4e4ef4fa1c018d7e46884b75
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-04-11 13:45:00 +0200

    Fix several issues detected by puppet-lint

--- Modulefile
+++ Modulefile
@@ -1,5 +1,5 @@
 name 'dhoppe-fail2ban'
-version '0.0.9'
+version '0.0.10'
 
 author 'Dennis Hoppe'
 license 'GPLv2'
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,57 +1,57 @@
 class fail2ban {
 	define fail2ban::email($email = false, $whitelist = false) {
 		$t_email = $email ? {
-			false   => "root",
+			false   => 'root',
 			default => $email,
 		}
 
 		$t_whitelist = $whitelist ? {
-			false   => ["127.0.0.1"],
+			false   => ['127.0.0.1'],
 			default => $whitelist,
 		}
 
 		file { "$name":
 			owner   => root,
 			group   => root,
-			mode    => 0644,
-			alias   => "jail.conf",
+			mode    => '0644',
+			alias   => 'jail.conf',
 			content => template("fail2ban/${::lsbdistcodename}/etc/fail2ban/jail.conf.erb"),
-			notify  => Service["fail2ban"],
-			require => Package["fail2ban"],
+			notify  => Service['fail2ban'],
+			require => Package['fail2ban'],
 		}
 	}
 
-	fail2ban::email { "/etc/fail2ban/jail.conf":
+	fail2ban::email { '/etc/fail2ban/jail.conf':
 		email     => hiera('email'),
 		whitelist => hiera_array('whitelist'),
 	}
 
-	file { "/etc/fail2ban/jail.local":
+	file { '/etc/fail2ban/jail.local':
 		owner   => root,
 		group   => root,
-		mode    => 0644,
-		alias   => "jail.local",
+		mode    => '0644',
+		alias   => 'jail.local',
 		source  => [
 			"puppet:///modules/fail2ban/${::hostname}/etc/fail2ban/jail.local",
-			"puppet:///modules/fail2ban/common/etc/fail2ban/jail.local"
+			'puppet:///modules/fail2ban/common/etc/fail2ban/jail.local'
 		],
-		notify  => Service["fail2ban"],
-		require => Package["fail2ban"],
+		notify  => Service['fail2ban'],
+		require => Package['fail2ban'],
 	}
 
-	package { "fail2ban":
+	package { 'fail2ban':
 		ensure => present,
 	}
 
-	service { "fail2ban":
-		enable     => true,
+	service { 'fail2ban':
 		ensure     => running,
+		enable     => true,
 		hasrestart => true,
 		hasstatus  => true,
 		require    => [
-			File["jail.conf"],
-			File["jail.local"],
-			Package["fail2ban"]
+			File['jail.conf'],
+			File['jail.local'],
+			Package['fail2ban']
 		],
 	}
 }
