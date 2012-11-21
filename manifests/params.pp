ff9d39e04d9006e124cb24246f90f053f9c247bb
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-11-21 16:35:40 +0100

    Remove support for Lenny    - End of Life on 2012-02-06

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class fail2ban::params {
   case $::lsbdistcodename {
-    'lenny', 'squeeze', 'maverick', 'natty': {
+    'squeeze', 'maverick', 'natty': {
       $email     = hiera('email')
       $whitelist = hiera('whitelist')
     }
