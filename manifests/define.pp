21653593778aa49d8f3457b6f10a82d6f3932f55
Dennis Hoppe <github@debian-solutions.de>
2014-11-09 13:39:02 +0100

    Change to top-scope variables

--- manifests/define.pp
+++ manifests/define.pp
@@ -12,7 +12,7 @@ define fail2ban::define (
   $config_file_notify       = undef,
   $config_file_require      = undef,
 
-  $config_file_options_hash = $fail2ban::config_file_options_hash,
+  $config_file_options_hash = $::fail2ban::config_file_options_hash,
 ) {
   if $config_file_path { validate_absolute_path($config_file_path) }
   if $config_file_owner { validate_string($config_file_owner) }
@@ -25,17 +25,17 @@ define fail2ban::define (
   if $config_file_notify { validate_string($config_file_notify) }
   if $config_file_require { validate_string($config_file_require) }
 
-  $_config_file_path  = pick($config_file_path, "${fail2ban::config_dir_path}/${name}")
-  $_config_file_owner = pick($config_file_owner, $fail2ban::config_file_owner)
-  $_config_file_group = pick($config_file_group, $fail2ban::config_file_group)
-  $_config_file_mode = pick($config_file_mode, $fail2ban::config_file_mode)
+  $_config_file_path  = pick($config_file_path, "${::fail2ban::config_dir_path}/${name}")
+  $_config_file_owner = pick($config_file_owner, $::fail2ban::config_file_owner)
+  $_config_file_group = pick($config_file_group, $::fail2ban::config_file_group)
+  $_config_file_mode = pick($config_file_mode, $::fail2ban::config_file_mode)
   $config_file_content = default_content($config_file_string, $config_file_template)
 
-  $_config_file_notify = pick($config_file_notify, $fail2ban::config_file_notify)
-  $_config_file_require = pick($config_file_require, $fail2ban::config_file_require)
+  $_config_file_notify = pick($config_file_notify, $::fail2ban::config_file_notify)
+  $_config_file_require = pick($config_file_require, $::fail2ban::config_file_require)
 
   file { "define_${name}":
-    ensure  => $fail2ban::config_file_ensure,
+    ensure  => $::fail2ban::config_file_ensure,
     path    => $_config_file_path,
     owner   => $_config_file_owner,
     group   => $_config_file_group,
