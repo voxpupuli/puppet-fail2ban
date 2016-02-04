afa3b8d5ee1655db7c31b584656e8beb34900840
Dennis Hoppe <github@debian-solutions.de>
2016-02-04 08:21:46 +0100

    Update configuration for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
+  c.include PuppetlabsSpec::Files
+
   c.before :each do
     # Ensure that we don't accidentally cache facts and environment
     # between test cases.
