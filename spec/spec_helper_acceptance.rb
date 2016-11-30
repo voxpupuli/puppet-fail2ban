38aa4b1c4af1ab041b774b65fc20a1e9f5f71b47
Dennis Hoppe <github@debian-solutions.de>
2016-11-30 12:54:59 +0100

    Add missing dependency for Beaker tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -18,6 +18,7 @@ RSpec.configure do |c|
     # Install module and dependencies
     hosts.each do |host|
       copy_module_to(host, source: proj_root, module_name: 'fail2ban')
+      on host, puppet('module install puppet-extlib'), acceptable_exit_codes: [0, 1]
       on host, puppet('module install puppetlabs-stdlib'), acceptable_exit_codes: [0, 1]
     end
   end
