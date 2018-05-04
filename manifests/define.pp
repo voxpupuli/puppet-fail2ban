d5b29d5c19eaed493218283c175e3f828fcc82f7
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-05-04 13:37:11 +0200

    Use Puppet 4 datatypes

--- manifests/define.pp
+++ manifests/define.pp
@@ -1,48 +1,30 @@
 # == Define: fail2ban::define
 #
 define fail2ban::define (
-  $config_file_path         = undef,
-  $config_file_owner        = undef,
-  $config_file_group        = undef,
-  $config_file_mode         = undef,
-  $config_file_source       = undef,
-  $config_file_string       = undef,
-  $config_file_template     = undef,
+  Stdlib::Absolutepath $config_file_path = "${fail2ban::config_dir_path}/${title}",
+  String $config_file_owner = $fail2ban::config_file_owner,
+  String $config_file_group = $fail2ban::config_file_group,
+  String $config_file_mode = $fail2ban::config_file_mode,
+  Optional[String] $config_file_source = undef,
+  Optional[String] $config_file_string = undef,
+  Optional[String] $config_file_template = undef,
 
-  $config_file_notify       = undef,
-  $config_file_require      = undef,
+  String $config_file_notify = $fail2ban::config_file_notify,
+  String $config_file_require = $fail2ban::config_file_require,
 
-  $config_file_options_hash = $::fail2ban::config_file_options_hash,
+  Hash $config_file_options_hash = $::fail2ban::config_file_options_hash,
 ) {
-  if $config_file_path { validate_absolute_path($config_file_path) }
-  if $config_file_owner { validate_string($config_file_owner) }
-  if $config_file_group { validate_string($config_file_group) }
-  if $config_file_mode { validate_string($config_file_mode) }
-  if $config_file_source { validate_string($config_file_source) }
-  if $config_file_string { validate_string($config_file_string) }
-  if $config_file_template { validate_string($config_file_template) }
-
-  if $config_file_notify { validate_string($config_file_notify) }
-  if $config_file_require { validate_string($config_file_require) }
-
-  $_config_file_path  = pick($config_file_path, "${::fail2ban::config_dir_path}/${name}")
-  $_config_file_owner = pick($config_file_owner, $::fail2ban::config_file_owner)
-  $_config_file_group = pick($config_file_group, $::fail2ban::config_file_group)
-  $_config_file_mode = pick($config_file_mode, $::fail2ban::config_file_mode)
   $config_file_content = default_content($config_file_string, $config_file_template)
-
-  $_config_file_notify = pick($config_file_notify, $::fail2ban::config_file_notify)
-  $_config_file_require = pick($config_file_require, $::fail2ban::config_file_require)
 
   file { "define_${name}":
     ensure  => $::fail2ban::config_file_ensure,
-    path    => $_config_file_path,
-    owner   => $_config_file_owner,
-    group   => $_config_file_group,
-    mode    => $_config_file_mode,
+    path    => $config_file_path,
+    owner   => $config_file_owner,
+    group   => $config_file_group,
+    mode    => $config_file_mode,
     source  => $config_file_source,
     content => $config_file_content,
-    notify  => $_config_file_notify,
-    require => $_config_file_require,
+    notify  => $config_file_notify,
+    require => $config_file_require,
   }
 }
--- manifests/jail.pp
+++ manifests/jail.pp
@@ -1,70 +1,39 @@
 # == Define: fail2ban::jail
 #
 define fail2ban::jail (
-  $filter_includes             = undef,
-  $filter_failregex            = undef,
-  $filter_ignoreregex          = undef,
-  $filter_additional_config    = undef,
-  $enabled                     = true,
-  $action                      = undef,
-  $filter                      = undef,
-  $logpath                     = undef,
-  $maxretry                    = $fail2ban::maxtretry,
-  $findtime                    = undef,
-  $bantime                     = $fail2ban::bantime,
-  $port                        = undef,
-  Optional[String] $backend    = undef,
+  Optional[String] $filter_includes = undef,
+  Optional[String] $filter_failregex = undef,
+  Optional[String] $filter_ignoreregex = undef,
+  $filter_additional_config = undef,
+  Boolean $enabled = true,
+  Optional[String] $action = undef,
+  String $filter = $title,
+  String $logpath = undef,
+  Integer $maxretry = $fail2ban::maxtretry,
+  Optional[Integer] $findtime = undef,
+  Integer $bantime = $fail2ban::bantime,
+  Optional[String] $port = undef,
+  Optional[String] $backend = undef,
 
-
-  $config_dir_filter_path   = $fail2ban::config_dir_filter_path,
-  $config_file_owner        = undef,
-  $config_file_group        = undef,
-  $config_file_mode         = undef,
-  $config_file_source       = undef,
-  $config_file_notify       = undef,
-  $config_file_require      = undef,
+  Stdlib::Absolutepath $config_dir_filter_path = $fail2ban::config_dir_filter_path,
+  Optional[String] $config_file_owner = $fail2ban::config_file_owner,
+  Optional[String] $config_file_group = $fail2ban::config_file_group,
+  Optional[String] $config_file_mode = $fail2ban::config_file_mode,
+  Optional[String] $config_file_source = $fail2ban::config_file_source,
+  Optional[String] $config_file_notify = $fail2ban::config_file_notify,
+  Optional[String] $config_file_require = $fail2ban::config_file_require,
 ) {
-
-  # Validation
-  if $filter_includes { validate_string($filter_includes) }
-  if $filter_failregex { validate_string($filter_failregex) }
-  if $filter_ignoreregex { validate_string($filter_ignoreregex) }
-  if $enabled { validate_bool($enabled) }
-  if $action { validate_string($action) }
-  if $filter { validate_string($filter) }
-  if $logpath { validate_string($logpath) }
-  else { fail('logpath required for each jail declaration') }
-  if $maxretry { validate_integer($maxretry) }
-  if $findtime { validate_integer($findtime) }
-  if $bantime { validate_integer($bantime) }
-  if $port { validate_string($port) }
-
-  if $config_dir_filter_path { validate_absolute_path($config_dir_filter_path) }
-  if $config_file_owner { validate_string($config_file_owner) }
-  if $config_file_group { validate_string($config_file_group) }
-  if $config_file_mode { validate_string($config_file_mode) }
-  if $config_file_notify { validate_string($config_file_notify) }
-  if $config_file_require { validate_string($config_file_require) }
-
-  # Value assignment
-  $_filter = pick($filter, $name)
-
-  $_config_file_owner = pick($config_file_owner, $::fail2ban::config_file_owner)
-  $_config_file_group = pick($config_file_group, $::fail2ban::config_file_group)
-  $_config_file_mode = pick($config_file_mode, $::fail2ban::config_file_mode)
-  $_config_file_notify = pick($config_file_notify, $::fail2ban::config_file_notify)
-  $_config_file_require = pick($config_file_require, $::fail2ban::config_file_require)
 
   # Jail filter creation
   file { "custom_filter_${name}":
     ensure  => file,
     path    => "${config_dir_filter_path}/${name}.conf",
     content => template('fail2ban/common/custom_filter.conf.erb'),
-    owner   => $_config_file_owner,
-    group   => $_config_file_group,
-    mode    => $_config_file_mode,
-    notify  => $_config_file_notify,
-    require => $_config_file_require,
+    owner   => $config_file_owner,
+    group   => $config_file_group,
+    mode    => $config_file_mode,
+    notify  => $config_file_notify,
+    require => $config_file_require,
   }
 
   # Jail creation
@@ -72,10 +41,10 @@ define fail2ban::jail (
     ensure  => file,
     path    => "${::fail2ban::params::config_dir_path}/jail.d/${name}.conf",
     content => template('fail2ban/common/custom_jail.conf.erb'),
-    owner   => $_config_file_owner,
-    group   => $_config_file_group,
-    mode    => $_config_file_mode,
-    notify  => $_config_file_notify,
-    require => $_config_file_require,
+    owner   => $config_file_owner,
+    group   => $config_file_group,
+    mode    => $config_file_mode,
+    notify  => $config_file_notify,
+    require => $config_file_require,
   }
 }
