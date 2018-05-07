ef3f3a2ae74d8d646fb987cb55c3ba9470a50efb
Cristian Schuszter <darkgeeky@gmail.com>
2018-05-07 15:33:49 +0200

    Add templates for ubuntu 18, fix CentOS 6, install EPEL before tests.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -13,10 +13,14 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module and dependencies
-    puppet_module_install(source: proj_root, module_name: 'fail2ban')
     hosts.each do |host|
+      if fact_on(host, 'osfamily') == 'RedHat'
+        on host, puppet('resource', 'package', 'epel-release', 'ensure=installed')
+        on host, puppet('resource', 'package', 'redhat-lsb-core', 'ensure=installed')
+      end
       on host, puppet('module', 'install', 'puppetlabs-stdlib'), acceptable_exit_codes: [0]
       on host, puppet('module', 'install', 'puppet-extlib'), acceptable_exit_codes: [0]
+      puppet_module_install(source: proj_root, module_name: 'fail2ban')
     end
   end
 end
