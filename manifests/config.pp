c64ab8d479c4d09c7b1d3dd2a0f11a804cc4c181
Steve Traylen <steve.traylen@cern.ch>
2018-05-08 15:51:11 +0200

    Manage /etc/fail2ban directory always
    Since this module attemts to maintain `/etc/fail2ban/fail2ban.conf`
    for the case `package_ensure` is `absent` it can not rely on the
    package maintaining the directory and the directory must  also
    be managed.
    
    Probably a lot of this `absent` vs `purged` should be ripped out.

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,17 +4,15 @@ class fail2ban::config {
   # Load custom jails definition
   $config_custom_jails = hiera_hash('fail2ban::custom_jails', undef)
 
-  if $::fail2ban::config_dir_source {
-    file { 'fail2ban.dir':
-      ensure  => $::fail2ban::config_dir_ensure,
-      path    => $::fail2ban::config_dir_path,
-      force   => $::fail2ban::config_dir_purge,
-      purge   => $::fail2ban::config_dir_purge,
-      recurse => $::fail2ban::config_dir_recurse,
-      source  => $::fail2ban::config_dir_source,
-      notify  => $::fail2ban::config_file_notify,
-      require => $::fail2ban::config_file_require,
-    }
+  file { 'fail2ban.dir':
+    ensure  => $::fail2ban::config_dir_ensure,
+    path    => $::fail2ban::config_dir_path,
+    force   => $::fail2ban::config_dir_purge,
+    purge   => $::fail2ban::config_dir_purge,
+    recurse => $::fail2ban::config_dir_recurse,
+    source  => $::fail2ban::config_dir_source,
+    notify  => $::fail2ban::config_file_notify,
+    require => $::fail2ban::config_file_require,
   }
 
   if $::fail2ban::config_file_path {
