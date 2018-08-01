26055de48284ad8511a33db47378342f0c2b077d
Arjen Zonneveld <a.zonneveld@bz2.nl>
2018-08-01 21:46:30 +0200

    Apply minimum lengths to String types
    Don't allow empty string as parameter values.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@
 #
 class fail2ban (
   Enum['absent', 'latest', 'present', 'purged'] $package_ensure = 'present',
-  String $package_name = $::fail2ban::params::package_name,
+  String[1] $package_name = $::fail2ban::params::package_name,
   Optional[Array[String]] $package_list = $::fail2ban::params::package_list,
 
   Stdlib::Absolutepath $config_dir_path = $::fail2ban::params::config_dir_path,
@@ -12,34 +12,34 @@ class fail2ban (
   Optional[String] $config_dir_source = undef,
 
   Stdlib::Absolutepath $config_file_path = $::fail2ban::params::config_file_path,
-  String $config_file_owner = $::fail2ban::params::config_file_owner,
-  String $config_file_group = $::fail2ban::params::config_file_group,
-  String $config_file_mode = $::fail2ban::params::config_file_mode,
-  String $config_file_before = $::fail2ban::params::before_file,
-  Optional[String]] $config_file_source = undef,
-  Optional[String]] $config_file_string = undef,
-  Optional[String]] $config_file_template = undef,
+  String[1] $config_file_owner = $::fail2ban::params::config_file_owner,
+  String[1] $config_file_group = $::fail2ban::params::config_file_group,
+  String[1] $config_file_mode = $::fail2ban::params::config_file_mode,
+  String[1] $config_file_before = $::fail2ban::params::before_file,
+  Optional[String[1]] $config_file_source = undef,
+  Optional[String[1]] $config_file_string = undef,
+  Optional[String[1]] $config_file_template = undef,
 
-  String $config_file_notify = $::fail2ban::params::config_file_notify,
-  String $config_file_require = $::fail2ban::params::config_file_require,
+  String[1] $config_file_notify = $::fail2ban::params::config_file_notify,
+  String[1] $config_file_require = $::fail2ban::params::config_file_require,
 
-  Hash[String, Any] $config_file_hash = {},
+  Hash[String[1], Any] $config_file_hash = {},
   Hash $config_file_options_hash = {},
 
   Enum['running', 'stopped'] $service_ensure = 'running',
-  String $service_name = $::fail2ban::params::service_name,
+  String[1] $service_name = $::fail2ban::params::service_name,
   Boolean $service_enable = true,
 
-  String $action = 'action_mb',
+  String[1] $action = 'action_mb',
   Integer[0] $bantime = 432000,
-  String $email = "fail2ban@${::domain}",
-  String $sender = "fail2ban@${::fqdn}",
-  String $iptables_chain = 'INPUT',
-  Array[String] $jails = ['ssh', 'ssh-ddos'],
+  String[1] $email = "fail2ban@${::domain}",
+  String[1] $sender = "fail2ban@${::fqdn}",
+  String[1] $iptables_chain = 'INPUT',
+  Array[String[1]] $jails = ['ssh', 'ssh-ddos'],
   Integer[0] $maxretry = 3,
   Array $whitelist = ['127.0.0.1/8', '192.168.56.0/24'],
   $custom_jails = undef,
-  String $banaction = 'iptables-multiport',
+  String[1] $banaction = 'iptables-multiport',
 ) inherits ::fail2ban::params {
   $config_file_content = default_content($config_file_string, $config_file_template)
 
