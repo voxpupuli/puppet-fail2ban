64408a4394c72542644b1dc9123f4d63ec342195
Arjen Zonneveld <a.zonneveld@bz2.nl>
2018-08-01 21:35:20 +0200

    Switch from $::osfamily to $facts['os']['family']

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,57 +1,57 @@
 # == Class: fail2ban::params
 #
 class fail2ban::params {
-  $package_name = $::osfamily ? {
+  $package_name = $facts['os']['family'] ? {
     default => 'fail2ban',
   }
 
-  $package_list = $::osfamily ? {
+  $package_list = $facts['os']['family'] ? {
     default => undef,
   }
 
-  $config_dir_path = $::osfamily ? {
+  $config_dir_path = $facts['os']['family'] ? {
     default => '/etc/fail2ban',
   }
 
-  $config_dir_filter_path = $::osfamily ? {
+  $config_dir_filter_path = $facts['os']['family'] ? {
     default => '/etc/fail2ban/filter.d',
   }
 
-  $config_file_path = $::osfamily ? {
+  $config_file_path = $facts['os']['family'] ? {
     default => '/etc/fail2ban/jail.conf',
   }
 
-  $config_file_owner = $::osfamily ? {
+  $config_file_owner = $facts['os']['family'] ? {
     default => 'root',
   }
 
-  $config_file_group = $::osfamily ? {
+  $config_file_group = $facts['os']['family'] ? {
     default => 'root',
   }
 
-  $config_file_mode = $::osfamily ? {
+  $config_file_mode = $facts['os']['family'] ? {
     default => '0644',
   }
 
-  $config_file_notify = $::osfamily ? {
+  $config_file_notify = $facts['os']['family'] ? {
     default => 'Service[fail2ban]',
   }
 
-  $config_file_require = $::osfamily ? {
+  $config_file_require = $facts['os']['family'] ? {
     default => 'Package[fail2ban]',
   }
 
-  $before_file = $::osfamily ? {
+  $before_file = $facts['os']['family'] ? {
     'Debian' => 'paths-debian.conf',
     'RedHat' => 'paths-fedora.conf',
     default  => 'paths-fedora.conf',
   }
 
-  $service_name = $::osfamily ? {
+  $service_name = $facts['os']['family'] ? {
     default => 'fail2ban',
   }
 
-  case $::osfamily {
+  case $facts['os']['family'] {
     'Debian': {
     }
     'RedHat': {
