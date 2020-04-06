e99fc47efc0df26e818dd9cb7ea42ed91350319d
Romuald Conty <romuald@opus-codium.fr>
2020-04-06 21:16:19 +0200

    Spec: Remove redhat-lsb-core dependency

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,8 +1,5 @@
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
-  if fact_on(host, 'osfamily') == 'RedHat'
-    host.install_package('epel-release')
-    host.install_package('redhat-lsb-core')
-  end
+  host.install_package('epel-release') if fact_on(host, 'osfamily') == 'RedHat'
 end
