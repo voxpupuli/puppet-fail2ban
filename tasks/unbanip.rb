955631626e380bddbbd99901d343d5b5576f114c
Romain Tartière <romain@blogreen.org>
2024-05-31 17:08:54 -1000

    Add tasks to ban/unban IP addresses in jails
    The fail2ban-client tool allows a large panel of acions.  Start with the
    most simple and frequently used ones to ban/unban IP addresses in
    fail2ban jails.
    
    Next time I lock me out of a system through SSH, I will be happy to rely
    on these tasks with choria to unlock me.

---
+++ tasks/unbanip.rb
@@ -0,0 +1,17 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+# frozen_string_literal: true
+
+require_relative '../../ruby_task_helper/files/task_helper'
+
+class Fail2banUnbanipTask < TaskHelper
+  def task(jail:, ips:, **_kwargs)
+    command = ['fail2ban-client', 'set', jail, 'unbanip'] + ips
+
+    pid = Process.spawn(*command)
+    Process.wait(pid)
+
+    nil
+  end
+end
+
+Fail2banUnbanipTask.run if $PROGRAM_NAME == __FILE__
