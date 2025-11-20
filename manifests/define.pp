f8bd279e1ca95446b183db437c08775e7b753b2e
Francesco Di Nucci <d1nuc0m@protonmail.com>
2025-11-20 10:09:51 +0100

    syle: param alignment

--- manifests/define.pp
+++ manifests/define.pp
@@ -2,17 +2,17 @@
 #
 define fail2ban::define (
   Stdlib::Absolutepath $config_file_path = "${fail2ban::config_dir_path}/${title}",
-  String $config_file_owner = $fail2ban::config_file_owner,
-  String $config_file_group = $fail2ban::config_file_group,
-  String $config_file_mode = $fail2ban::config_file_mode,
-  Optional[String] $config_file_source = undef,
-  Optional[String] $config_file_string = undef,
-  Optional[String] $config_file_template = undef,
+  String               $config_file_owner = $fail2ban::config_file_owner,
+  String               $config_file_group = $fail2ban::config_file_group,
+  String               $config_file_mode = $fail2ban::config_file_mode,
+  Optional[String]     $config_file_source = undef,
+  Optional[String]     $config_file_string = undef,
+  Optional[String]     $config_file_template = undef,
 
-  String $config_file_notify = $fail2ban::config_file_notify,
-  String $config_file_require = $fail2ban::config_file_require,
+  String               $config_file_notify = $fail2ban::config_file_notify,
+  String               $config_file_require = $fail2ban::config_file_require,
 
-  Hash $config_file_options_hash = $fail2ban::config_file_options_hash,
+  Hash                 $config_file_options_hash = $fail2ban::config_file_options_hash,
 ) {
   $config_file_content = extlib::default_content($config_file_string, $config_file_template)
 
