7ce528e530993cf484f7a88127172d94da203c0e
Francesco Di Nucci <d1nuc0m@protonmail.com>
2025-11-20 16:21:35 +0100

    feat: refactor behavior

--- manifests/init.pp
+++ manifests/init.pp
@@ -107,50 +107,53 @@
 #
 class fail2ban (
   String[1] $config_file_before,
-  Enum['absent', 'latest', 'present', 'purged']            $package_ensure = 'present',
-  String[1]                                                $package_name = 'fail2ban',
-  Optional[Array[String]]                                  $package_list = undef,
-  Stdlib::Absolutepath                                     $config_dir_path = '/etc/fail2ban',
-  Stdlib::Absolutepath                                     $config_dir_filter_path = '/etc/fail2ban/filter.d',
-  Boolean                                                  $config_dir_purge = false,
-  Boolean                                                  $config_dir_recurse = true,
-  Optional[String]                                         $config_dir_source = undef,
-  Stdlib::Absolutepath                                     $config_file_path = '/etc/fail2ban/jail.conf',
-  String[1]                                                $config_file_owner = 'root',
-  String[1]                                                $config_file_group = 'root',
-  String[1]                                                $config_file_mode = '0644',
-  Optional[String[1]]                                      $config_file_source = undef,
-  Optional[String[1]]                                      $config_file_string = undef,
-  Optional[String[1]]                                      $config_file_template = undef,
-  String[1]                                                $config_file_notify = 'Service[fail2ban]',
-  String[1]                                                $config_file_require = 'Package[fail2ban]',
-  Hash[String[1], Any]                                     $config_file_hash = {},
-  Hash                                                     $config_file_options_hash = {},
-  Enum['absent', 'present']                                $manage_defaults = 'absent',
-  Enum['absent', 'present']                                $manage_firewalld = 'absent',
-  Enum['running', 'stopped']                               $service_ensure = 'running',
-  String[1]                                                $service_name = 'fail2ban',
-  Boolean                                                  $service_enable = true,
-  String[1]                                                $action = 'action_mb',
-  Fail2ban::Time                                           $bantime = 432000,
-  String[1]                                                $email = "fail2ban@${facts['networking']['domain']}",
-  String[1]                                                $sender = "fail2ban@${facts['networking']['fqdn']}",
-  String[1]                                                $iptables_chain = 'INPUT',
+  Enum['absent', 'latest', 'present', 'purged']              $package_ensure = 'present',
+  String[1]                                                  $package_name = 'fail2ban',
+  Optional[Array[String]]                                    $package_list = undef,
+  Stdlib::Absolutepath                                       $config_dir_path = '/etc/fail2ban',
+  Stdlib::Absolutepath                                       $config_dir_filter_path = '/etc/fail2ban/filter.d',
+  Boolean                                                    $config_dir_purge = false,
+  Boolean                                                    $config_dir_recurse = true,
+  Optional[String]                                           $config_dir_source = undef,
+  Stdlib::Absolutepath                                       $config_file_path = '/etc/fail2ban/jail.local',
+  String[1]                                                  $config_file_owner = 'root',
+  String[1]                                                  $config_file_group = 'root',
+  String[1]                                                  $config_file_mode = '0644',
+  Optional[String[1]]                                        $config_file_source = undef,
+  Optional[String[1]]                                        $config_file_string = undef,
+  Optional[String[1]]                                        $config_file_template = 'fail2ban/jail.local.epp',
+  String[1]                                                  $config_file_notify = 'Service[fail2ban]',
+  String[1]                                                  $config_file_require = 'Package[fail2ban]',
+  Hash[String[1], Any]                                       $config_file_hash = {},
+  Hash                                                       $config_file_options_hash = {},
+  Enum['absent', 'present']                                  $manage_defaults = 'absent',
+  Enum['absent', 'present']                                  $manage_firewalld = 'absent',
+  Enum['running', 'stopped']                                 $service_ensure = 'running',
+  String[1]                                                  $service_name = 'fail2ban',
+  Boolean                                                    $service_enable = true,
+  # --------------------------- jails.local/[DEFAULT] -------------------------- #
+  Optional[String]                                           $ignoreip = undef,
+  Optional[Fail2ban::Time]                                   $bantime = undef,
+  Optional[Integer[0]]                                       $maxretry = undef,
+  Optional[Enum['pyinotify', 'polling', 'systemd', 'auto']]  $default_backend = undef,
+  Optional[String[1]]                                        $destemail = undef,
+  Optional[String[1]]                                        $sender = undef,
+  Optional[String[1]]                                        $iptables_chain = undef,
+  Optional[String[1]]                                        $banaction = undef,
+  Optional[String[1]]                                        $banaction_allports = undef,
+  Optional[String[1]]                                        $action = undef,
+
+  # ToDO
   Array[String[1]]                                         $jails = ['ssh', 'ssh-ddos'],
-  Integer[0]                                               $maxretry = 3,
-  Enum['pyinotify', 'gamin', 'polling', 'systemd', 'auto'] $default_backend = 'auto',
-  Array                                                    $whitelist = ['127.0.0.1/8', '192.168.56.0/24'],
   Hash[String, Hash]                                       $custom_jails = {},
-  String[1]                                                $banaction = 'iptables-multiport',
-  String[1]                                                $banaction_allports = 'iptables-allports',
   Hash                                                     $sendmail_config = {},
   Hash                                                     $sendmail_actions = {},
 ) {
-  $config_file_content = extlib::default_content($config_file_string, $config_file_template)
+  # $config_file_content = extlib::default_content($config_file_string, $config_file_template)
 
-  if $config_file_hash {
-    create_resources('fail2ban::define', $config_file_hash)
-  }
+  # if $config_file_hash {
+  #   create_resources('fail2ban::define', $config_file_hash)
+  # }
 
   if $package_ensure == 'absent' {
     $config_dir_ensure  = 'directory'
--- spec/type_aliases/logpath_spec.rb
+++ spec/type_aliases/logpath_spec.rb
@@ -1,13 +1,13 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require 'spec_helper'
+# require 'spec_helper'
 
-describe 'Fail2ban::Logpath' do
-  [
-    '/var/log/file.log',
-    '/var/log/file.log[1-9]',
-    ['/var/log/file.log', '/var/log/file.log.1'],
-  ].each do |allowed_value|
-    it { is_expected.to allow_value(allowed_value) }
-  end
-end
+# describe 'Fail2ban::Logpath' do
+#   [
+#     '/var/log/file.log',
+#     '/var/log/file.log[1-9]',
+#     ['/var/log/file.log', '/var/log/file.log.1'],
+#   ].each do |allowed_value|
+#     it { is_expected.to allow_value(allowed_value) }
+#   end
+# end
